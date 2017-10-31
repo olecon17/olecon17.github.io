13e01643c31b379239112a546ea4836dbbee9d3e
Conor OLeary <oleary.conor17@gmail.com>
2017-10-31 16:14:25 +0000

    Restore contact form validation

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,34 +1,60 @@
 var $contactForm = $('#contactForm');
-$contactForm.submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-        url: '//formspree.io/olearyconor17@gmail.com',
-        method: 'POST',
-        data: $(this).serialize(),
-        dataType: 'json',
-                success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
+$(function () {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
         },
-        error: function() {
-          // Fail message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                .append("</button>");
-            $('#success > .alert-success')
-                .append("<strong>Error. </strong>");
-            $('#success > .alert-success')
-                .append('</div>');
-            //clear all fields
-            $('#contactForm').trigger("reset");
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+
+            $.ajax({
+                url: '//formspree.io/olearyconor17@gmail.com',
+                method: 'POST',
+                data: $contactForm.serialize(),
+                dataType: 'json',
+                success: function () {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Error. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                }
+            });
         },
+        filter: function () {
+            return $(this).is(":visible");
+        }
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+
     });
 });
